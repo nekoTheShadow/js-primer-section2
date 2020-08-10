13acaad3ebc56b07b120703769d60624173a0a03
hajime nakamura <h.nakamura0903@gmail.com>
2020-08-10 21:27:54 +0900

    31.3.1. fsモジュールを使ってファイルを読み込む - エラーハンドリング

--- nodecli/main.js
+++ nodecli/main.js
@@ -5,5 +5,10 @@ program.parse(process.argv)
 const filePath = program.args[0]
 
 fs.readFile(filePath, {encoding: 'utf-8'}, (err, file) => {
+  if (err) {
+    console.log(err.message)
+    process.exit(1)
+    return 
+  }
   console.log(file)
 })
