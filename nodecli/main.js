02dbb5de774f36b60384174b601d651a3c117967
hajime nakamura <h.nakamura0903@gmail.com>
2020-08-10 21:36:13 +0900

    31.4.2 変換オプションを作成する - コマンドライン引数からオプションを受け取る

--- nodecli/main.js
+++ nodecli/main.js
@@ -2,8 +2,10 @@ const program = require('commander')
 const fs = require('fs')
 const marked = require('marked')
 
+program.option('--gfm', 'GFMを有効にする')
 program.parse(process.argv)
 const filePath = program.args[0]
+const options = program.opts()
 
 fs.readFile(filePath, {encoding: 'utf-8'}, (err, file) => {
   if (err) {
