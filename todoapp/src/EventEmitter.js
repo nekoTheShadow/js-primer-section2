dcd443ad89b7dae8a2c1a41aa2024c365a93214f
hajime nakamura <h.nakamura0903@gmail.com>
2020-08-11 23:14:49 +0900

    32.4.4 EventEmitter

---
+++ todoapp/src/EventEmitter.js
@@ -0,0 +1,34 @@
+export class EventEmitter {
+  constructor() {
+    this._listeners = new Map()
+  }
+
+  addEventListener(type, listener) {
+    if (!this._listeners.has(type)) {
+      this._listeners.set(type, new Set())
+    }
+    this.listener.get(type).add(listener)
+  }
+
+  emit(type) {
+    const listenerSet = this._listeners.get(type)
+    if (!listenerSet) {
+      return ;
+    }
+    listenerSet.forEach(listener => {
+      listener.call(this)
+    });
+  }
+
+  removeEventListener(type, listener) {
+    const listenerSet = this._listeners.get(type)
+    if (!listenerSet) {
+      return
+    }
+    listenerSet.forEach(ownListener => {
+      if (ownListener === listener) {
+        listenerSet.delete(listener)
+      }
+    })
+  }
+}
